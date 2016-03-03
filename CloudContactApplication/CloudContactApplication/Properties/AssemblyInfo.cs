3db76ede5de49f932812a280506fef5dc76c650b
Will Hogan <willhogan11@hotmail.com>
2016-03-03 10:14:05 +0000

    Restructured / Refactored files

--- CloudContactApplication/CloudContactApplication/Properties/AssemblyInfo.cs
+++ CloudContactApplication/CloudContactApplication/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CloudContactApplication")]
+[assembly: AssemblyTitle("Shopping Helper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CloudContactApplication")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Shopping Helper")]
+[assembly: AssemblyCopyright("Copyright © Will Hogan 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
